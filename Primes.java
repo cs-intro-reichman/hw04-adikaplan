727f509c16cc03623aad75a4983d049ee6eec741
Adi Kaplan <adikaplan@MacBook-Air-sl-Adi.local>
2025-11-22 00:16:50 +0200

    completed Q2

--- Primes.java
+++ Primes.java
@@ -1,5 +1,36 @@
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
+        int n = Integer.parseInt(args[0]);
+        if(n<2){
+            System.out.println("No Prime Numbers");
+            return;
+        }
+        int sqrtN = (int)Math.sqrt(n);
+        boolean[] arr = new boolean[n+1];
+        for(int i = 2; i< n+1 ; i++){
+             arr[i] = true;  
+        }
+        int p =2;
+        while( p <= sqrtN){
+            for(int i = p+1; i < n+1;i++){
+        if(i % p == 0 && arr[i] == true){
+                arr[i] = false;
+            }
+        }
+        p++;
+        while(arr[p] == false){
+            p++;
+        }
+    }
+    System.out.println("Prime numbers up to " + n + ":");
+    int counter = 0;
+    for(int i = 2; i< n+1; i++){
+        if(arr[i] == true){
+            System.out.println(i);
+            counter++;
+        }
+    }
+    int precent = (counter* 100)/(n);
+    System.out.println("There are " + counter + " primes between 2 and " + n +" (" + precent +  "% are primes"+ ")");
     }
 }

ed68243693161549d473257a0ba2d432d904f97b
Adi Kaplan <adikaplan@MacBook-Air-sl-Adi.local>
2025-11-25 23:11:20 +0200

    completed Q3

--- MyString.java
+++ MyString.java
@@ -34,6 +34,9 @@ public class MyString {
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
+        if(str1 == null || str2 == null){
+            return false;
+        }
         if(str1.length() < str2.length()){
             return false;
         }
